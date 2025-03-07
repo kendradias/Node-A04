623d71697a5dd8e446809e20beef9f38006581b9
Kendra Dias <kendra.dias@hotmail.com>
2025-03-07 12:22:10 -0800

    adjusted renderhelper.js

--- src/utils/renderHelpers.js
+++ src/utils/renderHelpers.js
@@ -1,11 +1,23 @@
-// Utility functions for rendering responses
+/**
+ * Utility functions for rendering responses
+ */
 
-// Render JSON response
+/**
+ * Render JSON response
+ * @param {Object} res - Express response object
+ * @param {Object} data - Data to be returned as JSON
+ * @param {Number} statusCode - HTTP status code
+ */
 const renderJson = (res, data, statusCode = 200) => {
     return res.status(statusCode).json(data);
 };
 
-// Render EJS page
+/**
+ * Render EJS page
+ * @param {Object} res - Express response object
+ * @param {String} template - Template name
+ * @param {Object} data - Data to be passed to the template
+ */
 const renderPage = (res, template, data = {}) => {
     // Don't override the layout system properties
     return res.render(template, {
